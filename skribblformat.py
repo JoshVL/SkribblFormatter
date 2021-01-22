e5b41551dd2877f83f49f9cceedfce8d6b81e846
Josh Villanueva <77131654+JoshVL@users.noreply.github.com>
2021-01-22 13:50:50 -0600

    Fixed empty word add and updated special chars

--- skribblformat.py
+++ skribblformat.py
@@ -3,19 +3,40 @@ Skribbl Formatter - Formats a text file to be used as a custom Skribbl.IO word l
 
 Josh Villanueva
 '''
-# Obtain text file to format
-fileName = input("Enter the txt file location: ")
-txtFile = open(fileName, "r")
+# Removing Special Characters
+def remChars(inWord):
+    inWord = inWord.replace(',', '')
+    inWord = inWord.replace('.', '')
+    inWord = inWord.replace('!', '')
+    inWord = inWord.replace('=', '')
+    inWord = inWord.replace(';', '')
+    inWord = inWord.replace(':', '')
+    
+    return inWord
 
-# Creates the formated txt file to put formated words
-formatedTxt = open("Formated.txt", "w+")
+# Main Function
+def main():
+    # Obtain text file to format
+    fileName = input("Enter the txt file location: ")
+    txtFile = open(fileName, "r")
 
-# Reads through the given txt file to format words into formated txt
-if txtFile.mode == 'r':
-    for line in txtFile:
-        for word in line.split():
-            formatedTxt.write(word + ", ")
+    # Creates the formated txt file to put formated words
+    formatedTxt = open("Formated.txt", "w+")
 
-# Closing the text files once finished
-txtFile.close()
-formatedTxt.close()
+    # Reads through the given txt file to format words into formated txt
+    if txtFile.mode == 'r':
+        for line in txtFile:
+            for word in line.split():
+                # Remove special characters
+                word = remChars(word)
+                # Does not write to txt if formated word is now empty
+                if not(word == ''):
+                    formatedTxt.write(remChars(word) + ", ")
+
+    # Closing the text files once finished
+    print("\nFormatted words successfully. Please check Formated.txt for your formated list of words.")
+    txtFile.close()
+    formatedTxt.close()
+
+if __name__ == "__main__":
+    main()
